0a8da64317eb89acd88d438b7d73f7bbfe65cf24
Aleksey <1862647@gmail.com>
2022-03-28 22:02:36 +0700

    section about desktop

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const swiperSectionAbout = new Swiper('.about__cards-container', {
+  
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   }, 
+   
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+
+   slidesPerView: 3,
+   watchOverflow: false,
+   spaceBetween: 32,
+   initialSlide: 0
+});
