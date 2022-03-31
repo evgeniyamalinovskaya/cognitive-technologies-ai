4ad3a0129cd52b75acc8f2ef11b6b73571e17e42
evgeniyamalinovskaya <malinovskayaevgeniya@yandex.com>
2022-03-31 22:57:36 +0500

    change header

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 export * from './sliders.js';
+
+//Бургерного меню
+
+const headerMenuButton = document.querySelector('.header__menu-button');
+const headerBurger = document.querySelector('.header__burger');
+const headerBurgerCloseButton = document.querySelector('.header__burger-close');
+
+// Функция открытия бургерного меню и закрытия главного меню
+
+headerMenuButton.addEventListener('click', function (event) {
+    headerBurger.classList.add('header__burger_opened');
+});
+
+headerMenuButton.addEventListener('click', function (event) {
+    headerBurger.classList.remove('header__container_hidden');
+});
+
+// Функция закрытия бургерного меню и открытия главного меню
+
+headerBurgerCloseButton.addEventListener('click', function (event) {
+    headerBurger.classList.add('header__container_hidden');
+});
+headerBurgerCloseButton.addEventListener('click', function (event) {
+    headerBurger.classList.remove('header__burger_opened');
+});
+
